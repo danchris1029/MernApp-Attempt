c43155fa86d5344f0433a65029137e8f73affd27
Christian Guardiola <61804256+danchris1029@users.noreply.github.com>
2021-05-24 18:24:12 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -7,16 +7,68 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const MONGODB_URI = 'mongodb+srv://chris:Merrychristmas@cluster0.0ze8b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(MONGODB_URI ||'mongodb://localhost/mern_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!');
+})
+
+// Schema
+
+const Schema = mongoose.Schema;
+const BlogPostSchema = new Schema({
+    title: String,
+    body: String,
+    date:{
+        type: String,
+        default: Date.now()
+    }
+});
+
+// Model
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+
+// Saving data to our mongo database
+const data = {
+    title: 'Welcome to my home',
+    body: 'I help folks with their homework'
+};
+
+const newBlogPost = new BlogPost(data); // instance of the model
+
+// newBlogPost.save((error) => {
+//     if (error){
+//         console.log('Ooops, something has happened');
+//     }else{
+//         console.log('Data has been saved!');
+//     }
+// });
+
 // HTTP request logger
 app.use(morgan('tiny'));
 
 // Routes
 app.get('/api/', (req, res) =>{
-    const data = {
-        username: 'accimesterlin',
-        age: 5
-    };
-    res.json(data);
+    // const data = {
+    //     username: 'accimesterlin',
+    //     age: 5
+    // };
+
+    BlogPost.find({})
+        .then((data) =>{
+            console.log('Data: ', data);
+            res.json(data);
+        })
+        .catch((error) =>{
+            console.log('error: ', daerrorta);
+        });
+
+    
 });
 
 app.get('/api/name', (req, res) =>{
