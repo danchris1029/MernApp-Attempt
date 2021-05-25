9b2d5b1038c3f79226ea510ee39cae4478c80c30
Christian Guardiola <61804256+danchris1029@users.noreply.github.com>
2021-05-25 02:58:00 -0500

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    title: '',
+    body: ''
+  };
+
+  handleChange = (event) =>{
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({
+      [name]: value
+    })
+  };
+
+  submit = (event) => {
+    event.preventDefault();
+
+    const payload = {
+      title: this.state.title,
+      body: this.state.body
+    };
+
+    axios({
+      url: 'http://localhost:8080/api/save',
+      method: 'POST',
+      data: payload
+    })
+      .then(() => {
+        console.log('Data has been sent to the server');
+      })
+      .catch(() => {
+        console.log('Internal server error');
+      });
+  }
+
+  render() {
+
+    console.log('State: ', this.state);
+    //JSX
+    return(
+      <div>
+        <h2> Welcome to my Appp</h2>
+        <form onSubmit={this.submit}>
+          <div className="form-input"></div>
+            <input 
+              type="text"
+              name="title"
+              placeholder="Title"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
+          <div className="form-input">
+            <textarea 
+              placeholder="body" 
+              name="body" 
+              cols="30" 
+              rows="10" 
+              value={this.state.body}
+              onChange={this.handleChange}
+            >
+            </textarea>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
